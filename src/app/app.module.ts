92df96810d88a3167b8122dd6e4b719785da96e3
Khang Tran <khangtrann8198@gmail.com>
2022-07-09 17:33:18 +0700

    Implement header & qr code functionality

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { QrCodeComponent } from './components/header/components/qr-code/qr-code.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    QrCodeComponent
   ],
   imports: [
     BrowserModule,
